211d2683d805c3a6c600d0d15afbee66b193e2ce
MathSena <MathSena07@hotmail.com>
2024-01-25 22:06:08 +0000

    Initial commit

---
+++ src/main/java/com/mathsena/musicalogapi/MusicalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.mathsena.musicalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicalogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathsena/musicalogapi/MusicalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.musicalogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
